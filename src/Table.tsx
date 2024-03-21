45195587ad2eaa92e1c9d85b501751ec60050913
Jeremy Steele <jeremy.ma.steele@gmail.com>
2024-03-21 15:10:10 -0400

    Full size table

--- src/Table.tsx
+++ src/Table.tsx
@@ -1,4 +1,5 @@
 import { FixedSizeGrid as Grid } from 'react-window';
+import AutoSizer from "react-virtualized-auto-sizer";
 import './Table.css';
 
 const Cell = ({ columnIndex, rowIndex, style }: { columnIndex: number, rowIndex: number, style: any }) => (
@@ -9,17 +10,22 @@ const Cell = ({ columnIndex, rowIndex, style }: { columnIndex: number, rowIndex:
 
 const Table = (results: any) => {
   return (
+
     <div className="table">
-      <Grid
-        columnCount={1000}
-        columnWidth={100}
-        height={500}
-        rowCount={1000}
-        rowHeight={35}
-        width={500}
-      >
-        {Cell}
-      </Grid>
+      <AutoSizer>
+        {({ height, width }: { height: number, width:number  }) => (
+          <Grid
+            columnCount={1000}
+            columnWidth={100}
+            height={height}
+            rowCount={1000}
+            rowHeight={35}
+            width={width}
+          >
+            {Cell}
+          </Grid>
+        )}
+      </AutoSizer>
     </div>
   );
 }
