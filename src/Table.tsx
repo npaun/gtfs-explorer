203681dccb0fc136ff287af926a72807d04b353e
Jeremy Steele <jeremy.ma.steele@gmail.com>
2024-03-21 14:56:08 -0400

    add table shell with react-window

--- src/Table.tsx
+++ src/Table.tsx
@@ -1,5 +1,27 @@
+import { FixedSizeGrid as Grid } from 'react-window';
+import './Table.css';
+
+const Cell = ({ columnIndex, rowIndex, style }: { columnIndex: number, rowIndex: number, style: any }) => (
+  <div style={style}>
+    Item {rowIndex},{columnIndex}
+  </div>
+);
+
 const Table = (results: any) => {
-  return (<div>Table</div>)
+  return (
+    <div className="table">
+      <Grid
+        columnCount={1000}
+        columnWidth={100}
+        height={500}
+        rowCount={1000}
+        rowHeight={35}
+        width={500}
+      >
+        {Cell}
+      </Grid>
+    </div>
+  );
 }
 
 export default Table;
