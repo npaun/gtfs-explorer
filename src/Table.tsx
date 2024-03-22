c2e856ab16c82c5ec77da43847ea3e505112574d
Nicholas Paun <nicholas@transit.app>
2024-03-22 09:52:09 -0700

    Try to switch to react-virtuoso

--- src/Table.tsx
+++ src/Table.tsx
@@ -1,58 +1,35 @@
-import BaseTable from 'react-base-table';
-import AutoSizer from 'react-virtualized-auto-sizer'
-import 'react-base-table/styles.css'
+import { TableVirtuoso } from 'react-virtuoso'
 import './Table.css';
 
-const columns = ['a', 'bbbbbb', 'c', 'd', 'e', 'fasdfdsfadsjfl;', 'g', 'h', 'i', 'j'].map((col,i) => ({key: col, dataKey: col, title: col, width: 0, flexGrow: 1}));
-
-const data2 = new Array(1000).fill(0).map((row, rowIndex) => {
-  return {
-    id: `row${rowIndex}`,
-    ...Object.fromEntries(columns.map((col) => [col.dataKey, `Row ${rowIndex} - Col ${col.dataKey}`]))
-  }
-});
-
-console.log(data2[0])
+const keyOf = (i: number, j: number) => `{$i}-${j}`;
 
 const Table = ({ sqlResult }: {sqlResult:{error:unknown}|{data:[{columns: string[]; values: unknown[][]}]} |null}) => {
   if (sqlResult && 'data' in sqlResult && sqlResult.data.length > 0 && sqlResult.data[0].values.length > 0) {
-    const { columns, data } = parseQueryResult(sqlResult.data[0]);
+    const columns = sqlResult.data[0].columns;
+    const values = sqlResult.data[0].values;
+
     return (
-      <AutoSizer>
-        {({ height, width }: { height: number, width: number }) => (
-          <BaseTable height={height} width={width} fixed={false} columns={columns} data={data}/>   
+      <div className="table">
+        <TableVirtuoso
+        style={{height: "80vh"}}
+        data={values}
+        totalCount={values.length}
+        fixedHeaderContent={() => (
+          <tr>
+            {columns.map((col) => (<th>{col}</th>))}
+          </tr>
         )}
-      </AutoSizer>
-    )
+        itemContent={(i, row) => (
+          row.map((value, j) => (
+            <td key={i*columns.length + j}>{value}</td>
+          ))
+        )}
+        />
+      </div>
+    );
   }
   return <div className="empty">No data</div>
 }
 
 export default Table;
 
-function parseQueryResult({ columns, values }: {columns: string[]; values: unknown[][]}) {
-  const colWidths = Array(columns.length).fill(0);
-  const convertedRows: ({[k: string]: string} & {id: string})[] = [];
-  let rowIndex = 0;
-  for (const row of values) {
-    let i = 0;
-    const rowConverted: ({[k: string]: string} & {id: string}) = {
-      id: `row-${rowIndex}`,
-    };
-    for (const item of row) {
-      const itemStr = String(item);
-      rowConverted[columns[i]] = itemStr;
-      if (itemStr.length > colWidths[i]) colWidths[i] = itemStr.length;
-      i += 1;
-    }
-    convertedRows.push(rowConverted);
-    rowIndex += 1;
-  }
-
-  const sumWidths = colWidths.reduce((sum, w) => sum + w, 0);
-
-  return {
-    columns: columns.map((col, i) => ({ key: col, dataKey: col, title: col, width: 0, flexGrow: colWidths[i] / sumWidths * 100 })), 
-    data: convertedRows
-  };
-}
